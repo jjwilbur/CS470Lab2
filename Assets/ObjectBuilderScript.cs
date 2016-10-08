32639dff0bbbcfa25037cc939cd777c3b859ecb0
Jordan Wilbur <Jordan Wilbur>
2016-10-07 21:31:02 -0600

    added maze scenes and additional scripts from TA

---
+++ Assets/ObjectBuilderScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class ObjectBuilderScript : MonoBehaviour 
+{
+
+
+	bool beenBuilt;
+
+	//Your grid stuff
+
+
+
+
+	//this gets called when you press the button in the unity inspector
+	public void BuildObject()
+		{beenBuilt = true;
+		//this is where you initialize all of your nodes/grid
+
+
+
+
+		}
+
+
+	void OnDrawGizmos()
+	{
+		if (beenBuilt) {
+
+			// Do a double for loop and draw all of your nodes in your grid
+
+			//Gizmos.DrawWireSphere (nodes[i, j].position, .5f);
+
+		}
+	}
+}
