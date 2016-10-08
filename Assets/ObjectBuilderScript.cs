30e741e8773a3d29fce4fec0e874d78f9c70f8c1
Jordan Wilbur <Jordan Wilbur>
2016-10-08 15:42:19 -0600

    fixed error plus working on grid

--- Assets/ObjectBuilderScript.cs
+++ Assets/ObjectBuilderScript.cs
@@ -8,17 +8,21 @@ public class ObjectBuilderScript : MonoBehaviour
 
 	bool beenBuilt;
 
-	//Your grid stuff
-
-
-
-
+    //Your grid stuff
+    public GameObject[,] nodes = new GameObject[10, 10];
 	//this gets called when you press the button in the unity inspector
+
 	public void BuildObject()
 		{beenBuilt = true;
-		//this is where you initialize all of your nodes/grid
+        //this is where you initialize all of your nodes/grid
 
+        for(var i = 0; i < 10; i++)
+        {
+            for(var j = 0; j < 10; j++)
+            {
 
+            }
+        }
 
 
 		}
@@ -29,9 +33,26 @@ public class ObjectBuilderScript : MonoBehaviour
 		if (beenBuilt) {
 
 			// Do a double for loop and draw all of your nodes in your grid
-
+            for(var i = 0; i < 10; i++)
+            {
+                for(var j = 0; j < 10; j++)
+                {
+                    //Gizmos.DrawWireSphere(nodes[i, j].location, 1.0f);
+                }
+            }
 			//Gizmos.DrawWireSphere (nodes[i, j].position, .5f);
 
 		}
 	}
+}
+
+public class Node : MonoBehaviour
+{
+    public Vector3 location;
+    public bool showNode;
+
+    public Vector3 getLocation()
+    {
+        return location;
+    }
 }
